61e7dd0f5b2946ae8df05d2ecc6f4d5213a953d2
rpattem <rajesh@taskvalue.in>
2024-05-06 21:29:19 +0530

    update Entities

--- src/main/java/com/example/userservice/entity/RegisteredUserCredentials.java
+++ src/main/java/com/example/userservice/entity/RegisteredUserCredentials.java
@@ -1,18 +1,23 @@
 package com.example.userservice.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class RegisteredUserCredentials {
     @Id
     @GeneratedValue
     private Long id;
 
-    @Lob
-    @Column(columnDefinition = "BLOB")
-    private byte[] password;
+    @Column(nullable = false)
+    private String password;
 
     @Lob
     @Column(columnDefinition = "BLOB")
