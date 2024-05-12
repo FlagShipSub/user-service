d79b6a34aaa70a1db0accac3bf3823998a9d50cd
Rithesh-Pothu <rithesh.441@gmail.com>
2024-05-12 10:39:57 +0530

    moved utility bean to Utils

--- src/main/java/com/example/userservice/ApplicationConfig.java
+++ src/main/java/com/example/userservice/ApplicationConfig.java
@@ -1,21 +1,15 @@
 package com.example.userservice;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.ui.freemarker.FreeMarkerConfigurationFactoryBean;
 
 @Configuration
 public class ApplicationConfig {
+
     @Value("${EMAIL_SENDER}")
     public String sender;
 
-    @Primary
-    @Bean
-    public FreeMarkerConfigurationFactoryBean factoryBean() {
-        FreeMarkerConfigurationFactoryBean bean=new FreeMarkerConfigurationFactoryBean();
-        bean.setTemplateLoaderPath("classpath:/templates");
-        return bean;
+    public String getSender() {
+        return sender;
     }
 }
