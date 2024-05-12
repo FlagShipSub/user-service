f15c387b31dc984558ba0559956afb23bc65165e
Rithesh-Pothu <rithesh.441@gmail.com>
2024-05-12 10:43:37 +0530

    removed unused env variable

--- src/main/java/com/example/userservice/ApplicationConfig.java
+++ src/main/java/com/example/userservice/ApplicationConfig.java
@@ -1,15 +1,8 @@
 package com.example.userservice;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ApplicationConfig {
-
-    @Value("${EMAIL_SENDER}")
-    public String sender;
-
-    public String getSender() {
-        return sender;
-    }
+    // use this class for using environment variables
 }
